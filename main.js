792f345d30a8531abcaa4d9a1448dba4c44fba3b
mikedubcurry <michaelcurry95@gmail.com>
2024-03-31 12:12:52 -0400

    classy

--- main.js
+++ main.js
@@ -1,26 +1,151 @@
 import "./style.css";
 
-const sequencerEl = document.querySelector("#sequencer");
-const playButton = document.querySelector("#play-button");
-const stopButton = document.querySelector("#stop-button");
-const clearButton = document.querySelector('#clear');
+class App {
+    constructor() {
+        this.sequencer = null;
+        this.synth = new Synth();
+        this.samples = {};
+        this.canvas = document.getElementById("wave");
+        this.ctx = this.canvas.getContext('2d')
+        this.w = 500;
+        this.h = 300;
+        this.canvas.width = this.w;
+        this.canvas.height = this.h;
+        this.ctx.fill = "#000000";
+        this.ctx.fillRect(0, 0, this.w, this.h);
+        this.frameId = null
+        this.keys = {}
+        this.notes = {
+            z: 261.63,
+            s: 277.18,
+            x: 293.66,
+            d: 311.13,
+            c: 329.63,
+            v: 349.23,
+            g: 369.99,
+            b: 392.00,
+            h: 415.30,
+            n: 440.00,
+            j: 466.16,
+            m: 493.88,
+            ",": 523.25,
+        }
+        this.arraySeq = new Uint8Array(1024);
+        this.arraySynth = new Uint8Array(1024);
+        this.array = new Uint8Array(1024);
 
-const samples = {};
+        const playButton = document.querySelector("#play-button");
+        const sequencerEl = document.querySelector("#sequencer");
+        const stopButton = document.querySelector("#stop-button");
+        const clearButton = document.querySelector('#clear');
 
-const notes = {
-    z: 261.63,
-    s: 277.18,
-    x: 293.66,
-    d: 311.13,
-    c: 329.63,
-    v: 349.23,
-    g: 369.99,
-    b: 392.00,
-    h: 415.30,
-    n: 440.00,
-    j: 466.16,
-    m: 493.88,
-    ",": 523.25,
+        this.draw = this.draw.bind(this)
+
+        playButton.addEventListener("click", (e) => {
+            if (!this.sequencer) this.sequencer = new Sequencer(this.samples);
+            this.sequencer.play();
+            if (this.sequencer.playing) {
+                e.target.innerText = "Pause";
+            } else {
+                e.target.innerText = "Play";
+            }
+            this.draw();
+            sequencerEl.innerHTML = this.renderList(
+                this.sequencer.steps.map((step, i) => this.renderStep(step, i))
+            );
+        });
+
+        stopButton.addEventListener("click", (e) => {
+            if (this.sequencer) {
+                this.sequencer.kill();
+                this.sequencer = null;
+                playButton.innerHTML = "Play";
+            }
+        });
+
+        clearButton.addEventListener('click', e => {
+            sequencerEl.innerHTML = this.renderList(
+                Array.from(Array(16)).map(_ => {
+                    return { notes: [0, 0, 0, 0] }
+                }).map(renderStep))
+        })
+
+        window.addEventListener('keydown', e => {
+            if (this.notes[e.key])
+                this.keys[e.key] = true;
+            this.playKeys();
+        })
+
+        window.addEventListener('keyup', e => {
+            if (this.notes[e.key])
+                this.keys[e.key] = false;
+            this.playKeys();
+        })
+
+        this.loadSamples().then(() => {
+            this.sequencer = new Sequencer(this.samples)
+        })
+    }
+
+    async loadSamples() {
+        const files = ["kick", "snare", "closed-hat", "open-hat"];
+        files.forEach(async (file) => {
+            const response = await fetch(`/samples/${file}.wav`);
+            const ctx = new AudioContext();
+            const arrayBuffer = await response.arrayBuffer();
+            const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
+
+            this.samples[file] = audioBuffer;
+        });
+    }
+
+    renderStep(step, index) {
+        return `
+            <div class='step'>
+            ${this.renderList(
+            step.notes.map((step) => `<input class='step' type='checkbox' data-step='${index}' ${step ? "checked" : ""} />`))}
+            </div>
+            `;
+    }
+
+    renderList(arr) {
+        return arr.join("");
+    }
+
+    draw() {
+        this.ctx.fillStyle = "#000000";
+        this.ctx.fillRect(0, 0, this.w, this.h);
+        this.synth.getWaveForm(this.arraySynth);
+        if (this.sequencer && this.sequencer.playing) {
+            this.sequencer.getWaveForm(this.arraySeq);
+        }
+        // average the two arrays
+        this.array.forEach((t, i) => {
+            this.array[i] = (this.arraySeq[i] + this.arraySynth[i]) / 2;
+        });
+        // if sequencer is not playing, tween the array to 0
+        if (this.sequencer && !this.sequencer.playing) {
+            this.array.forEach((t, i) => {
+                this.array[i] = t * 0.9;
+            });
+        }
+        this.array.forEach((t, i) => {
+            this.ctx.fillStyle = "#ffffff";
+            this.ctx.fillRect((i * this.w) / 1024, (t * (this.h / 2)) / 128, 2, 2);
+        });
+
+        this.frameId = requestAnimationFrame(this.draw);
+
+    }
+    playKeys() {
+        Object.keys(this.keys).map(k => {
+            if (this.keys[k] && this.notes[k]) {
+                this.synth.play(this.notes[k]);
+            } else {
+                this.synth.stop(this.notes[k]);
+            }
+        })
+    }
 }
 
 class Synth {
@@ -28,14 +153,9 @@ class Synth {
         this.ctx = new AudioContext();
         this.oscs = {};
         this.preOut = new AnalyserNode(this.ctx);
-        //this.osc = this.ctx.createOscillator();
         this.gain = this.ctx.createGain();
-        //this.osc.connect(this.gain);
         this.preOut.connect(this.ctx.destination);
         this.gain.connect(this.preOut);
-        //this.osc.start(0);
-        //this.osc.type = "square";
-        //this.osc.frequency.value = 440;
         this.gain.gain.value = 1;
     }
 
@@ -51,15 +171,11 @@ class Synth {
             this.oscs[note].gain.connect(this.gain);
             this.oscs[note].osc.connect(this.oscs[note].gain);
             this.oscs[note].osc.start(0);
-            this.oscs[note].osc.type = "triangle";
-            this.oscs[note].osc.frequency.value = note / 2;
+            this.oscs[note].osc.type = "sawtooth";
+            this.oscs[note].osc.frequency.value = note / 4;
         } else {
             this.oscs[note].gain.gain.value = 0.8;
         }
-        //if (note) {
-        //    this.osc.frequency.value = note;
-        //    this.gain.gain.value = 0.5;
-        //}
     }
 
     stop(note) {
@@ -229,131 +345,4 @@ class Sequencer {
     }
 }
 
-let sequencer = null;
-let synth = new Synth();
-
-playButton.addEventListener("click", (e) => {
-    if (!sequencer) sequencer = new Sequencer(samples);
-    sequencer.play();
-    if (sequencer.playing) {
-        e.target.innerText = "Pause";
-    } else {
-        e.target.innerText = "Play";
-    }
-    draw();
-    sequencerEl.innerHTML = renderList(
-        sequencer.steps.map((step, i) => renderStep(step, i))
-    );
-});
-
-stopButton.addEventListener("click", (e) => {
-    if (sequencer) {
-        sequencer.kill();
-        sequencer = null;
-        playButton.innerHTML = "Play";
-    }
-});
-
-clearButton.addEventListener('click', e => {
-    sequencerEl.innerHTML = renderList(
-        Array.from(Array(16)).map(_ => {
-            return { notes: [0, 0, 0, 0] }
-        }).map(renderStep))
-})
-
-async function loadSamples() {
-    const files = ["kick", "snare", "closed-hat", "open-hat"];
-    files.forEach(async (file) => {
-        const response = await fetch(`/samples/${file}.wav`);
-        const ctx = new AudioContext();
-        const arrayBuffer = await response.arrayBuffer();
-        const audioBuffer = await ctx.decodeAudioData(arrayBuffer);
-        samples[file] = audioBuffer;
-    });
-}
-
-loadSamples();
-
-function renderStep(step, index) {
-    return `
-  <div class='step'>
-    ${renderList(
-        step.notes.map(
-            (step) => `
-      <input class='step' type='checkbox' data-step='${index}' ${step ? "checked" : ""} />
-      `
-        )
-    )}
-  </div>
-  `;
-}
-
-function renderList(arr) {
-    return arr.join("");
-}
-
-let keys = {}
-
-window.addEventListener('keydown', e => {
-    if (notes[e.key])
-        keys[e.key] = true;
-    playKeys();
-})
-
-window.addEventListener('keyup', e => {
-    if (notes[e.key])
-        keys[e.key] = false;
-    playKeys();
-})
-
-function playKeys() {
-    Object.keys(keys).map(k => {
-        if (keys[k] && notes[k]) {
-            synth.play(notes[k]);
-        } else {
-            synth.stop(notes[k]);
-        }
-    })
-}
-
-const canvas = document.getElementById("wave");
-const ctx = canvas.getContext("2d");
-let w = 500;
-let h = 300;
-canvas.width = w;
-canvas.height = h;
-ctx.fill = "#000000";
-ctx.fillRect(0, 0, w, h);
-
-
-const arraySeq = new Uint8Array(1024);
-const arraySynth = new Uint8Array(1024);
-const array = new Uint8Array(1024);
-
-let frameId = null;
-
-function draw() {
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(0, 0, w, h);
-    synth.getWaveForm(arraySynth);
-    if (sequencer && sequencer.playing) {
-        sequencer.getWaveForm(arraySeq);
-    }
-    // average the two arrays
-    array.forEach((t, i) => {
-        array[i] = (arraySeq[i] + arraySynth[i]) / 2;
-    });
-    // if sequencer is not playing, tween the array to 0
-    if (sequencer && !sequencer.playing) {
-        array.forEach((t, i) => {
-            array[i] = t * 0.9;
-        });
-    }
-    array.forEach((t, i) => {
-        ctx.fillStyle = "#ffffff";
-        ctx.fillRect((i * w) / 1024, (t * (h / 2)) / 128, 2, 2);
-    });
-
-    frameId = requestAnimationFrame(draw);
-
-}
+const app = new App()
